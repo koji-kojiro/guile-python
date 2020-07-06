4df658d2bd4e8cc525839f7d32417124e196062c
TANI Kojiro <kojiro0531@gmail.com>
2020-07-06 22:58:41 +0900

    Fix `#.` syntax to work properly

--- module/python/import.scm
+++ module/python/import.scm
@@ -1,4 +1,6 @@
 (define-module (python import)
+  #:use-module (oop goops)
+  #:use-module (srfi srfi-1)
   #:use-module (ice-9 eval-string)
   #:use-module (python eval)
   #:use-module (python core type)
@@ -10,8 +12,9 @@
     ((import module)
      (define-public
        module
-       (python-eval
-         (format #f "__import__(\"~a\")" (quote module)))))
+       (begin
+         (python-exec (format #f "import ~a as _" (quote module)))
+         (python-eval "_"))))
     ((import module #:as alias)
      (define-public
        alias
@@ -30,16 +33,28 @@
          (format #f "__import__(\"~a\", fromlist=[\"~a\"]).~a"
                 (quote module) (quote object) (quote object)))))))
 
+(define-method (getattr obj attr)
+  (format #t "~s . ~s\n" obj attr)
+  (python->scm (python-getattr-string (scm->python obj) attr)))
+
+(define-method (getattr (name <string>) attr)
+  (format #t "~s . ~s\n" name attr)
+  (with-exception-handler
+    (lambda (_)
+      (eval-string (format #f "~a.~a" name attr)))
+    (lambda () (getattr (eval-string name) attr))
+    #:unwind? #t))
+
+(define (test obj attr)
+  (format #t "~s . ~s\n" attr obj))
+
 (define (read-python-syntax _ p)
   (let* ((names (string-split (symbol->string (read p)) #\.))
-         (obj (eval-string (car names)))
-         (attributes (cdr names)))
+         (obj (car names)))
     (for-each
-      (lambda (name)
-        (set! obj
-              (python->scm
-                (python-getattr-string (scm->python obj) name))))
-      attributes)
+      (lambda (attr)
+        (set! obj (getattr obj attr)))
+      (cdr names))
     (if (list? obj) `(list ,@obj) obj)))
 
 (read-hash-extend #\. read-python-syntax)
