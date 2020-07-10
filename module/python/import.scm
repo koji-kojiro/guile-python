814f11ddb0a157321eebeac6552a4a6614d0e5f9
TANI Kojiro <kojiro0531@gmail.com>
2020-07-11 02:51:17 +0900

    Simplify `import` and make `#.` syntax work properly

--- module/python/import.scm
+++ module/python/import.scm
@@ -1,55 +1,24 @@
 (define-module (python import)
-  #:use-module (oop goops)
-  #:use-module (srfi srfi-1)
-  #:use-module (ice-9 eval-string)
-  #:use-module (python eval)
   #:use-module (python core type)
   #:use-module (python core libpython)
+  #:use-module (ice-9 optargs)
   #:replace (import))
 
-(define-syntax import
-  (syntax-rules ()
-    ((import module)
-     (define-public
-       module
-       (begin
-         (python-exec (format #f "import ~a as _" (quote module)))
-         (python-eval "_"))))
-    ((import module #:as alias)
-     (define-public
-       alias
-       (python-eval
-         (format #f "__import__(\"~a\")" (quote module)))))
-    ((import object #:from module)
-     (define-public
-       object
-       (python-eval
-         (format #f "__import__(\"~a\", fromlist=[\"~a\"]).~a"
-                (quote module) (quote object) (quote object)))))
-    ((import object #:from module #:as alias)
-     (define-public
-       alias
-       (python-eval
-         (format #f "__import__(\"~a\", fromlist=[\"~a\"]).~a"
-                (quote module) (quote object) (quote object)))))))
+(defmacro* import (module)
+  `(define-public
+     ,(string->symbol (car (string-split (symbol->string as) #\.)))
+       ((@ (python eval) python-eval) 
+         (format #f "__import__(\"~a\")" (quote ,module)))))
 
-(define-method (getattr obj attr)
-  (python->scm (python-getattr-string (scm->python obj) attr)))
-
-(define-method (getattr (name <string>) attr)
-  (with-exception-handler
-    (lambda (_)
-      (eval-string (format #f "~a.~a" name attr)))
-    (lambda () (getattr (eval-string name) attr))
-    #:unwind? #t))
+(define (getattr obj . attrs)
+  (for-each
+    (lambda (attr)
+      (set! obj (python->scm (python-getattr-string (scm->python obj) attr))))
+    attrs)
+  (if (list? obj) `(list ,@obj) obj))
 
 (define (read-python-syntax _ p)
-  (let* ((names (string-split (symbol->string (read p)) #\.))
-         (obj (car names)))
-    (for-each
-      (lambda (attr)
-        (set! obj (getattr obj attr)))
-      (cdr names))
-    (if (list? obj) `(list ,@obj) obj)))
+  (let ((names (string-split (symbol->string (read p)) #\.)))
+    `((@@ (python import) getattr) ,(string->symbol (car names)) ,@(cdr names))))
 
 (read-hash-extend #\. read-python-syntax)
