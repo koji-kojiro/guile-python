ae246c94a4f2a6fe093f12ee1beef30941da16a0
TANI Kojiro <kojiro0531@gmail.com>
2020-07-19 13:58:30 +0900

    Allow `set!` to set attributes of <python-object>

--- module/python/import.scm
+++ module/python/import.scm
@@ -1,8 +1,9 @@
 (define-module (python import)
   #:use-module (python core type)
   #:use-module (python core libpython)
+  #:use-module (oop goops)
   #:use-module (ice-9 string-fun)
-  #:replace (import))
+  #:replace (import set!))
 
 (define (scm-name->py-name name)
   (string-replace-substring name "-" "_"))
@@ -17,7 +18,10 @@
 (define (getattr obj . attrs)
   (for-each
     (lambda (attr)
-      (set! obj (python->scm (python-getattr-string (scm->python obj) attr))))
+      (let ((value (python->scm (python-getattr-string (scm->python obj) attr))))
+        (set-object-property! value #:parent obj)
+        (set-object-property! value #:name attr)
+        ((@ (guile) set!) obj value)))
     (map scm-name->py-name attrs))
   (if (list? obj) `(,@obj) obj))
 
@@ -25,4 +29,17 @@
   (let ((names (string-split (symbol->string (read p)) #\.)))
     `((@@ (python import) getattr) ,(string->symbol (car names)) ,@(cdr names))))
 
+(define (setattr obj value)
+  (python-setattr-string
+    (scm->python (object-property obj #:parent))
+    (object-property obj #:name)
+    (scm->python value)))
+
+(define-syntax-rule (set! obj value)
+  (let ((parent (object-property obj #:parent))
+        (name (object-property obj #:name)))
+    (if (and parent name)
+        ((@@ (python import) setattr) obj value)
+        ((@ (guile) set!) obj value))))
+ 
 (read-hash-extend #\. read-python-syntax)
